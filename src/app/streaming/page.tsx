b4560f2b78f93cae539929ad45f96971b97add3e
Joao Victor <joaovictor665@gmail.com>
2023-06-12 11:59:24 -0300

    teste2

--- src/app/streaming/page.tsx
+++ src/app/streaming/page.tsx
@@ -12,90 +12,87 @@ export default async function Streaming() {
   const modal = useRef<HTMLDivElement>(null);
   const router = useRouter();
 
-  let isLocal;
-  if (!window) {
-    isLocal = false;
-  }
+  if (typeof window !== "undefined") {
+    const isLocal = !!~localhost.indexOf(window.location.hostname);
 
-  isLocal = !!~localhost.indexOf(window.location.hostname);
+    const host = isLocal ? MANIFEST_URL.localHost : MANIFEST_URL.productionHost;
 
-  const host = isLocal ? MANIFEST_URL.localHost : MANIFEST_URL.productionHost;
+    const network = new Network(host);
 
-  const network = new Network(host);
+    let videoMediaPlayer = new VideoMediaPlayer({
+      manifestJSON: MANIFEST_URL,
+      network,
+    });
 
-  let videoMediaPlayer = new VideoMediaPlayer({
-    manifestJSON: MANIFEST_URL,
-    network,
-  });
+    useEffect(() => {
+      videoMediaPlayer.initializeCodec(videoRef, modal);
 
-  useEffect(() => {
-    videoMediaPlayer.initializeCodec(videoRef, modal);
+      videoRef.current?.addEventListener("play", closeModal);
 
-    videoRef.current?.addEventListener("play", closeModal);
+      return () => {
+        videoMediaPlayer.clearInterval();
+        videoRef.current?.removeEventListener("play", closeModal);
+      };
+    }, []);
 
-    return () => {
-      videoMediaPlayer.clearInterval();
-      videoRef.current?.removeEventListener("play", closeModal);
-    };
-  }, []);
+    const closeModal = useCallback(() => {
+      if (modal.current) {
+        modal.current.style.display = "none";
+      }
+    }, []);
 
-  const closeModal = useCallback(() => {
-    if (modal.current) {
-      modal.current.style.display = "none";
-    }
-  }, []);
+    const getBackRouter = useCallback(() => {
+      router.back();
+    }, [videoMediaPlayer]);
 
-  const getBackRouter = useCallback(() => {
-    router.back();
-  }, [videoMediaPlayer]);
-
-  return (
-    <div
-      className="
-        h-screen
-        w-screen
-      bg-black
-      "
-    >
-      <nav
+    return (
+      <div
         className="
-          fixed
-          w-full
-          p-4
-          z-10
-          flex
-          flex-row
-          items-center
-          gap-8
-          bg-black
-          bg-opacity-70
+          h-screen
+          w-screen
+        bg-black
         "
       >
-        <AiOutlineArrowLeft
-          className="text-white cursor-pointer"
-          size={40}
-          onClick={getBackRouter}
-        />
-        <p className=" text-white text-1xl md:text-3xl font-bold ">
-          <span className=" font-light ">
-            Whatching: Bandersnatch Experience
-          </span>
-        </p>
-      </nav>
-      <video ref={videoRef} autoPlay className="h-full w-full"></video>
-      <div
-        ref={modal}
-        className={`
-        fixed
-        flex
-        bottom-1/4   
-        w-full
-        h-auto
-        justify-center
-        items-center
-        gap-[30vw] 
-        `}
-      ></div>
-    </div>
-  );
+        <nav
+          className="
+            fixed
+            w-full
+            p-4
+            z-10
+            flex
+            flex-row
+            items-center
+            gap-8
+            bg-black
+            bg-opacity-70
+          "
+        >
+          <AiOutlineArrowLeft
+            className="text-white cursor-pointer"
+            size={40}
+            onClick={getBackRouter}
+          />
+          <p className=" text-white text-1xl md:text-3xl font-bold ">
+            <span className=" font-light ">
+              Whatching: Bandersnatch Experience
+            </span>
+          </p>
+        </nav>
+        <video ref={videoRef} autoPlay className="h-full w-full"></video>
+        <div
+          ref={modal}
+          className={`
+          fixed
+          flex
+          bottom-1/4   
+          w-full
+          h-auto
+          justify-center
+          items-center
+          gap-[30vw] 
+          `}
+        ></div>
+      </div>
+    );
+  }
 }
